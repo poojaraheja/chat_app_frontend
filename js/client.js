736f419948b0135ba67d37ae082acebc9f38cba3
poojaraheja <rahejapooja670@gmal.com>
2023-03-23 12:36:34 +0530

    code

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:8000");
+const socket = io("https://chat-app-p7d6.onrender.com");
 const form = document.getElementById("send-container");
 const messageInput = document.getElementById("messageInp");
 const messageContainer = document.querySelector(".container");
